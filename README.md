5e6b64f71c550688cc5af30e2ff71ecf1c83251d
Nedu2022 <nwabuokeichinedu00@gmail.com>
2022-09-02 22:48:46 +0100

    First commit 🚀

---
+++ README.md
@@ -0,0 +1,1 @@
+Flexing Javascript is a tutorial on how to get familiar with Javascript syntax. The tutorial was lectured by Jonas Schmedtamnn. Getting ready to smash JavaScript 🚀
---
+++ app.js
@@ -0,0 +1,65 @@
+// Practices
+let js = "amazing";
+
+// if (js === 'amazing') {
+//   alert('Javascript is Fun!');
+// }
+
+console.log("Nedu");
+console.log(600 + 50 * 4 * 425);
+
+let firstName = "Nedu";
+console.log(firstName);
+
+let myFirstJob = "Software Engineer";
+let myCurrentJob = "Frontend Developer";
+
+console.log(myFirstJob);
+
+let ballingIsFun = true;
+console.log(ballingIsFun);
+
+console.log(typeof true);
+console.log(typeof ballingIsFun);
+console.log(typeof 17);
+
+const userName = "Chinedu";
+const lastName = "Nwabuokei";
+console.log(userName + " " + lastName);
+
+const now = 2050;
+const ageNedu = now - 2004;
+const ageIfe = now - 2006;
+
+console.log(ageNedu);
+console.log(ageIfe);
+
+console.log(now - ageNedu > now - ageIfe);
+
+//1st Challenge 
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const BMIMark = markMass / markHeight ** 2;
+// const BMIJohn = johnMass / (johnHeight * johnHeight);
+
+// console.log(BMIMark, BMIJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(markHigherBMI);
+
+//2nd Challenge
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const BMIMark = markMass / markHeight ** 2;
+const BMIJohn = johnMass / (johnHeight * johnHeight);
+
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
